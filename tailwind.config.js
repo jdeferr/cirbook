2d2f38951789cc53d8d3e9a9f12ef309e15ef83d
Javier Deferr <jdeferr@gmail.com>
2024-02-03 20:01:29 +1300

    add: Add Font Roboto Condensed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         primary: '#F3F2EE',
         'primary-100': '#EDEFEC',
         secondary: '#DBAB5F',
+        'secondary-100': 'rgba(219,171,95,.24)',
         complementary: '#655D52',
         'book-bg': '#DEDEDE',
         'book-bg-100': '#FFFFFF',
@@ -14,7 +15,8 @@ export default {
       },
       fontFamily: {
         title: ['Quando', 'Open Sans', 'sans-serif'],
-        body: ['Open Sans', 'sans-serif']
+        body: ['Open Sans', 'sans-serif'],
+        price: ['Roboto Condensed', 'sans-serif']
       },
       maxWidth: {
         desktop: '1200px'

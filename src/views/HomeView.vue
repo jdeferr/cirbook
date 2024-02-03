c3c8b034e0db7877aae8bd405f635b70fc4a8980
Javier Deferr <jdeferr@gmail.com>
2024-02-03 20:02:24 +1300

    add: Add book list to home to mockup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BookList from '@components/Molecules/BookList.vue'
+</script>
 
 <template>
-  <main></main>
+  <main class="py-10">
+    <section class="max-w-desktop mx-auto">
+      <h4 class="font-title text-3xl text-center mb-10">Books</h4>
+      <div class="grid grid-cols-4 gap-x-24 gap-y-10">
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+        <BookList />
+      </div>
+    </section>
+  </main>
 </template>

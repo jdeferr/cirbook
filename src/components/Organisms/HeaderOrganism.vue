e9e9b5e7d1b2dd93d8c6e301154345fb2d4cc33e
Javier Deferr <jdeferr@gmail.com>
2024-02-03 15:01:33 +1300

    add: Add styles and estructure to HeaderOrgnaism

--- src/components/Organisms/HeaderOrganism.vue
+++ src/components/Organisms/HeaderOrganism.vue
@@ -2,15 +2,23 @@
 
 <template>
   <header class="bg-primary">
-    <div>
-      <h1>CIRBOOK</h1>
-    </div>
-    <div></div>
-    <div>
-      <ul>
-        <li>home</li>
-        <li>books</li>
-      </ul>
+    <div class="max-w-desktop h-16 flex justify-between items-center mx-auto">
+      <div>
+        <h1 class="font-title text-3xl">CIRBOOK</h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search by author, title, isbn..."
+          class="rounded-full w-[400px] py-2 px-5 font-extralight max-w-full"
+        />
+      </div>
+      <div>
+        <ul class="flex gap-x-5 text-lg font-light">
+          <li>home</li>
+          <li>books</li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>

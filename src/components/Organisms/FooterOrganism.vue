afbb294c40ee9518d046fd087dc0a9afa3341b7a
Javier Deferr <jdeferr@gmail.com>
2024-02-03 15:00:57 +1300

    add: Create footer orgnaism

---
+++ src/components/Organisms/FooterOrganism.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="bg-complementary text-white">
+    <div class="max-w-desktop py-10 pb-56 flex justify-between items-start mx-auto">
+      <div>
+        <h3 class="font-title text-xl font-light">Useful links</h3>
+        <hr class="my-2 w-[300px] max-w-full" />
+        <ul class="font-extralight">
+          <li>Home</li>
+          <li>Books</li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="font-title text-xl font-light">Developer links</h3>
+        <hr class="my-2 w-[300px] max-w-full" />
+        <ul class="font-extralight">
+          <li><a href="#">Linkedin</a></li>
+          <li><a href="#">Resume</a></li>
+        </ul>
+      </div>
+      <div>
+        <h1 class="font-title text-3xl">CIRBOOK</h1>
+      </div>
+    </div>
+  </header>
+</template>

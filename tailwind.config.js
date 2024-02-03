22b64a80eeb58811d18f9484e1012f649817b55b
Javier Deferr <jdeferr@gmail.com>
2024-02-03 13:13:26 +1300

    add: Fonts and colors variables to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    fontFamily: {
+      title: ['Quando', 'Open Sans', 'sans-serif'],
+      body: ['Open Sans', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: '#F3F2EE',
+        'primary-100': '#EDEFEC',
+        secondary: '#DBAB5F',
+        complementary: '#655D52',
+        'book-bg': '#DEDEDE',
+        'book-bg-100': '#FFFFFF',
+        author: '#A1A1A1'
+      }
+    }
   },
   plugins: []
 }

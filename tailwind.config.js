e15af52ae9a11bc0cf4c052bdc1c1eaec9807a07
Javier Deferr <jdeferr@gmail.com>
2024-02-06 21:04:22 +1300

    update: Update colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,32 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#F3F2EE',
-        'primary-100': '#EDEFEC',
-        secondary: '#DBAB5F',
-        'secondary-100': 'rgba(219,171,95,.24)',
+        primary: {
+          50: '#FDFDFC',
+          100: '#FDFDFC',
+          200: '#FBFAF9',
+          300: '#F9F8F6',
+          400: '#F4F4F0',
+          500: '#F3F2EE',
+          600: '#CAC6B4',
+          700: '#A49D7E',
+          800: '#726B50',
+          900: '#393528',
+          950: '#1B1913'
+        },
+        secondary: {
+          50: '#FBF6EE',
+          100: '#F8EEDE',
+          200: '#F1DEC1',
+          300: '#E9CD9F',
+          400: '#E2BC7E',
+          500: '#DBAB5F',
+          600: '#CC8F2E',
+          700: '#9A6C23',
+          800: '#684917',
+          900: '#32230B',
+          950: '#191206'
+        },
         complementary: '#655D52',
         'book-bg': '#DEDEDE',
         'book-bg-100': '#FFFFFF',

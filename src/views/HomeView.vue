cf7b02465657e4781bfb575d96fdb89a11e7566b
Javier Deferr <jdeferr@gmail.com>
2024-02-06 21:42:07 +1300

    add: Logic of store to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import BookList from '@components/Molecules/BookList.vue'
+import { useBookStore } from '@/stores/books'
+
+const bookStore = useBookStore()
+bookStore.getBooks()
 </script>
 
 <template>
   <main class="py-10">
     <section class="max-w-desktop mx-auto">
       <h4 class="font-title text-3xl text-center mb-10">Books</h4>
-      <div class="grid grid-cols-4 gap-x-24 gap-y-10">
+      <div v-if="bookStore.isLoading">Loading books</div>
+      <div
+        class="grid grid-cols-4 gap-x-24 gap-y-10"
+        v-else-if="!bookStore.isLoading && bookStore.allBooks.length > 0"
+      >
         <BookList />
         <BookList />
         <BookList />
@@ -19,6 +27,9 @@ import BookList from '@components/Molecules/BookList.vue'
         <BookList />
         <BookList />
         <BookList />
+      </div>
+      <div v-else>
+        <p>No books found: {{ bookStore.getError }}</p>
       </div>
     </section>
   </main>

72c62ad5af93ddbf385a744b75ca229a81aa4479
Javier Deferr <jdeferr@gmail.com>
2024-02-10 23:00:50 +1300

    add: Create Molecule column for footer

---
+++ src/components/Molecules/FooterColumnMolecule.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Link } from '@/entities/ui'
+import ListLinkAtomVue from '@/components/Atoms/ListLinkAtom.vue'
+
+const props = defineProps<{
+  title: string
+  links: Link[]
+}>()
+</script>
+
+<template>
+  <div>
+    <h3 class="font-title text-xl font-light">{{ props.title }}</h3>
+    <hr class="my-2 w-[300px] max-w-full" />
+    <ul class="font-extralight">
+      <ListLinkAtomVue
+        v-for="(link, key) in props.links"
+        :key="key"
+        :url="link.url"
+        :name="link.name"
+        :type="link.type"
+      />
+    </ul>
+  </div>
+</template>

ddc4957feb41f31d6849c2569a5b1d698012e592
Javier Deferr <jdeferr@gmail.com>
2024-02-10 13:53:13 +1300

    modify: Remove purchase logic and add interval fetch to books
    - Move purchase logic to store
    - Add interval book fetch, with this approach i could use books for the  book page, and if the las fetch was to old, i force fetch book to get a new possible stock

--- src/stores/books.ts
+++ src/stores/books.ts
@@ -6,6 +6,8 @@ const API_BOOK_URL = `${API_BASE_URL}/books`
 
 interface BooksState {
   books: Book[]
+  lastBooksFetch: Date | null
+  minFetchInterval: number
   book: Book | null
   loading: boolean
   error: Error | any
@@ -14,6 +16,8 @@ interface BooksState {
 export const useBookStore = defineStore('books', {
   state: (): BooksState => ({
     books: [],
+    lastBooksFetch: null,
+    minFetchInterval: 10 * 60 * 1000,
     book: null,
     loading: false,
     error: null
@@ -23,7 +27,14 @@ export const useBookStore = defineStore('books', {
     allBooks: (state): Book[] => state.books,
     currentBook: (state): Book | null => state.book,
     isLoading: (state): boolean => state.loading,
-    getError: (state): Error | any => state.error
+    getError: (state): Error | any => state.error,
+    shouldFetchBooks: (state) => {
+      const timeSinceLastFetch = state.lastBooksFetch
+        ? Date.now() - state.lastBooksFetch.getTime()
+        : Number.MAX_VALUE
+
+      return timeSinceLastFetch > state.minFetchInterval
+    }
   },
 
   actions: {
@@ -43,27 +54,20 @@ export const useBookStore = defineStore('books', {
     },
     async getBook(id: string) {
       this.loading = true
+      const book = this.books.find((book) => book.id === parseInt(id))
+      if (book && !this.shouldFetchBooks) {
+        this.book = book
+        this.loading = false
+        return
+      }
       try {
         const response = await fetch(`${API_BOOK_URL}/${id}`)
         this.book = (await response.json()).book
+        this.lastBooksFetch = new Date()
       } catch (error: any) {
         this.error = error
       } finally {
         this.loading = false
-      }
-    },
-    async purchaseBook(id: string) {
-      const response = await fetch(`${API_BOOK_URL}/${id}/purchase`, {
-        method: 'POST'
-      })
-      const data = await response.json()
-      if (response.status === 200) {
-        this.book = data.book
-        return data.message
-      } else if (response.status === 404 || response.status === 500) {
-        throw new Error(data.message)
-      } else {
-        throw new Error('Error desconocido')
       }
     }
   }

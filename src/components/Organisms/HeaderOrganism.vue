aa64e1fa3bca1d683026d8ea62f36eb8b7bb66d3
Javier Deferr <jdeferr@gmail.com>
2024-02-10 23:01:41 +1300

    add: Add logic to search by input

--- src/components/Organisms/HeaderOrganism.vue
+++ src/components/Organisms/HeaderOrganism.vue
@@ -1,23 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LogoAtom from '@/components/Atoms/LogoAtom.vue'
+import InputAtom from '@/components/Atoms/InputAtom.vue'
+import { useRouter } from 'vue-router'
+import { useBookStore } from '../../stores/books'
+
+const router = useRouter()
+const bookStore = useBookStore()
+
+const onKeyPressed = ($event: any) => {
+  // if ($event === '')
+  bookStore.setQuery($event)
+  router.push({ path: '/home', query: { search: $event } })
+}
+
+const onClickLogo = () => {
+  bookStore.setQuery(null)
+  router.push({ path: '/' })
+}
+</script>
 
 <template>
   <header class="bg-primary-500">
     <div class="max-w-desktop h-16 flex justify-between items-center mx-auto">
-      <div>
-        <router-link to="/">
-          <h1 class="font-title text-3xl">CIRBOOK</h1>
-        </router-link>
+      <div class="cursor-pointer" @click="onClickLogo">
+        <LogoAtom />
       </div>
       <div>
-        <input
+        <InputAtom
           type="text"
           placeholder="Search by author, title, isbn..."
-          class="rounded-full w-[400px] py-2 px-5 font-extralight max-w-full"
+          @on-key-pressed="onKeyPressed"
         />
       </div>
       <div>
         <ul class="flex gap-x-5 text-lg font-light">
-          <li>home</li>
           <li>books</li>
         </ul>
       </div>

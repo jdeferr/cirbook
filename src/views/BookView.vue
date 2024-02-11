13c40685041a3afa079b8e16a13aca11010fcf33
Javier Deferr <jdeferr@gmail.com>
2024-02-11 16:39:15 +1300

    fix: Responsive issue on views

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -22,20 +22,22 @@ const purchaseBook = () => {
 
 <template>
   <section class="bg-primary-500 mb-10" v-if="bookStore && bookStore.book">
-    <div class="max-w-desktop gap-x-10 mx-auto grid grid-cols-12 pt-10 translate-y-10">
-      <div class="col-start-3 col-span-3">
+    <div
+      class="px-5 md:max-w-[90%] lg:max-w-desktop gap-x-10 md:gap-x-5 lg:gap-x-10 mx-auto grid grid-cols-12 pt-2 md:pt-5 lg:pt-10 translate-y-2 md:translate-y-5 lg:translate-y-10"
+    >
+      <div class="col-span-4 md:col-start-3 md:col-span-3">
         <img
           :src="cover"
           :alt="bookStore.book.title"
           class="w-full overflow-hidden rounded-xl shadow-xl"
         />
       </div>
-      <div class="col-span-4 flex flex-col justify-around">
-        <div class="flex flex-col gap-y-10">
+      <div class="col-span-7 md:col-span-4 flex flex-col justify-around">
+        <div class="flex flex-col gap-y-1 md:gap-y-5 lg:gap-y-10">
           <div>
             <h5
               data-test="book-title"
-              class="font-body font-medium text-[40px] truncate-2-lines overflow-hidden"
+              class="font-body font-medium text-xl lg:leading-tight lg:text-[40px] truncate-2-lines overflow-hidden text-wrap"
             >
               {{ bookStore.book.title }}
             </h5>
@@ -47,7 +49,7 @@ const purchaseBook = () => {
             </p>
           </div>
           <div class="flex flex-col text-secondary-500 font-medium font-price">
-            <span class="text-3xl text-nowrap rounded-3xl"
+            <span class="text-xl md:text-3xl text-nowrap rounded-3xl"
               ><span data-test="book-price">{{ bookStore.book.price }}</span> NZD</span
             >
             <span
@@ -60,7 +62,7 @@ const purchaseBook = () => {
           <button
             v-if="bookStore.book.availableStock > 0"
             data-test="purchase-button"
-            class="font-button shadow-xl bg-complementary text-white rounded-full py-3 px-4 text-body"
+            class="font-button shadow-xl bg-complementary text-white rounded-full py-2 px-3 lg:py-3 lg:px-4 text-xs md:text-lg lg:text-xl text-body"
             @click="purchaseBook"
           >
             PURCHASE A COPY
@@ -68,7 +70,7 @@ const purchaseBook = () => {
           <button
             v-else
             data-test="out-of-stock-button"
-            class="font-button shadow-xl bg-gray-300 text-gray-500 rounded-full py-3 px-4 text-body"
+            class="font-button shadow-xl bg-gray-300 text-gray-500 rounded-full py-2 px-3 lg:py-3 lg:px-4 text-xs md:text-lg lg:text-xl text-body"
             disabled
           >
             OUT OF STOCK
@@ -77,9 +79,9 @@ const purchaseBook = () => {
       </div>
     </div>
   </section>
-  <section class="my-32 max-w-desktop mx-auto">
-    <h4 class="font-title text-3xl text-center mb-10">Synopsis</h4>
-    <p class="font-title text-lg font-extralight text-center">
+  <section class="my-20 lg:my-32 px-5 md:max-w-[90%] lg:max-w-desktop mx-auto">
+    <h4 class="font-title text-2xl md:text-3xl md:text-center mb-5 md:mb-10">Synopsis</h4>
+    <p class="text-md md:text-lg font-extralight md:text-center">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, temporibus exercitationem.
       Sed, repellat iure. Corporis totam nulla ut error aliquam laudantium cumque ducimus,
       voluptates dolore. Ad dignissimos ex numquam deleniti! Lorem ipsum, dolor sit amet consectetur
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,11 +15,11 @@ bookStore.getBooks()
 
 <template>
   <main class="py-10" v-if="bookStore">
-    <section class="max-w-desktop mx-auto">
+    <section class="px-2 md:max-w-[90%] lg:max-w-desktop mx-auto">
       <h4 class="font-title text-3xl text-center mb-10">Books</h4>
       <div data-test="loading-message" v-if="bookStore.isLoading">Loading books</div>
       <div
-        class="grid grid-cols-4 gap-x-24 gap-y-10"
+        class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-5 gap-x-5 md:gap-x-12 xl:gap-x-24"
         v-else-if="!bookStore.isLoading && bookStore.allBooks.length > 0"
       >
         <template v-for="book in bookStore.allBooks" :key="book.id">
